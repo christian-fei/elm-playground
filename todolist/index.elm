3f324f4a95fdb5fb901060237b4cfde0b5d0d84a
Christian Fei <crifei93@gmail.com>
2016-06-25 14:22:15 +0200

    can add a todo

--- todolist/index.elm
+++ todolist/index.elm
@@ -18,6 +18,7 @@ type alias Todo =
 
 type alias Model =
   { todos : List Todo
+  , todoInput : String
   }
 
 type Msg =
@@ -25,7 +26,7 @@ type Msg =
   | TodoInputChanged String
 
 init : (Model, Cmd Msg)
-init = (Model [], Cmd.none)
+init = (Model [] "", Cmd.none)
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
@@ -33,14 +34,18 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     TodoInputChanged text ->
-      (model, Cmd.none)
-    _ ->
-      (model, Cmd.none)
+      ({model | todoInput = text}, Cmd.none)
+    AddTodo text ->
+      ({model | todos = (addTodo text model.todos)}, Cmd.none)
+addTodo : String -> List Todo -> List Todo
+addTodo text todos =
+  todos ++ [{text = text}]
 view : Model -> Html Msg
 view model =
   main' []
     (List.concat
-      [[(h1 [] [text "Todolist"])]
-      , (List.map (\x -> (div [] [text x.text])) model.todos)
-      , [(input [onInput TodoInputChanged] [])]]
+      [ [h1 [] [text "Todolist"]]
+      , List.map (\x -> (div [] [text x.text])) model.todos
+      , [input [onInput TodoInputChanged] []
+        , button [onClick (AddTodo model.todoInput)] [text "Add todo"]]]
     )
