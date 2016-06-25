b2388cee5025e292a3f7eabd560ae81090906b41
Christian Fei <crifei93@gmail.com>
2016-06-25 14:05:19 +0200

    render todos

--- todolist/index.elm
+++ todolist/index.elm
@@ -1,5 +1,6 @@
 import Html exposing (..)
 import Html.App as Html
+import List
 
 main : Program Never
 main =
@@ -10,17 +11,29 @@ main =
   , view = view
   }
 
-type alias Model = ()
+type alias Todo =
+  String
 
-type Msg = Noop
+type alias TodoList = List Todo
+
+type alias Model =
+  { todos : TodoList
+  }
+
+type Msg =
+  AddTodo String
 
 init : (Model, Cmd Msg)
-init =
-  ((), Cmd.none)
+init = (Model ["hey", "wassup"], Cmd.none)
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = (model, Cmd.none)
 view : Model -> Html Msg
-view model = text "Todolist"
+view model =
+  main' []
+    (List.concat
+      [[(h1 [] [text "Todolist"])]
+      , (List.map (\x -> (div [] [text x])) model.todos)]
+    )
