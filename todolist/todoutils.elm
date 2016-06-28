e6a9e5c6ba6c99230b355cce7732c4926032b0bc
Christian Fei <crifei93@gmail.com>
2016-06-28 15:33:22 +0200

    fix todoutils

--- todolist/todoutils.elm
+++ todolist/todoutils.elm
@@ -2,10 +2,10 @@ module TodoUtils exposing (addTodo, alreadyPresentTodo)
 import Todo exposing (..)
 import List
 
-addTodo : Todo -> List Todo -> List Todo
+addTodo : Todo.Model -> List Todo.Model -> List Todo.Model
 addTodo todo todos =
   todos ++ [todo]
 
-alreadyPresentTodo : String -> List Todo -> Bool
+alreadyPresentTodo : String -> List Todo.Model -> Bool
 alreadyPresentTodo text todos =
   List.member text (List.map .text todos)
