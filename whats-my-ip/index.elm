6a7b1c2ff5595258b8a06171b2134509d6aca08a
Christian Fei <crifei93@gmail.com>
2016-09-06 15:01:14 +0200

    wip request ip

--- whats-my-ip/index.elm
+++ whats-my-ip/index.elm
@@ -2,7 +2,10 @@
 import Html exposing(..)
 --import Html.Events exposing(..)
 --import Html.Attributes exposing(value)
+import Http
 import TimeTravel.Html.App as TimeTravel
+import Json.Decode as Json exposing ((:=))
+import Task
 
 main : Program Never
 main =
@@ -19,7 +22,9 @@ type alias Model =
   }
 
 type Msg =
-  None
+  DoNothing
+  | RequestIP
+  | UpdateIP (Maybe String)
 
 init : (Model, Cmd Msg)
 init = (Model "Unknown IP", Cmd.none)
@@ -31,7 +36,24 @@ subscriptions model =
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
-    _ -> (model, Cmd.none)
+    RequestIP -> (model, requestIp)
+    _         -> (model, Cmd.none)
 
 view : Model -> Html Msg
 view model = text "42"
+
+
+---
+jsonIpUrl = "http://jsonip.com"
+
+requestIp : Cmd Msg
+requestIp =
+  Task.map UpdateIP (Task.toMaybe (Http.get extractIpField "http://jsonip.com"))
+  --Http.get ("ip" := Json.string) "http://jsonip.com"
+  --  |> Task.toMaybe
+  --  |> Task.map UpdateIP
+  --  |> Task
+
+extractIpField : Json.Decoder String
+extractIpField =
+  Json.at ["ip"] Json.string
